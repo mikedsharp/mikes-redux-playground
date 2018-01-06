2dfed4bd9b72ab734ffc84a2667a067358b0c337
Mike Sharp <msharp23@hotmail.co.uk>
2018-01-06 12:29:54 +0000

    babies first reducer function

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,10 +3,28 @@ var redux = require('redux');
 console.log('starting redux...');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
-    return state;
+    console.log('new action', action);
+    switch (action.type) {
+        case 'CHANGE_NAME': {
+            return {
+                ...state,
+                name: action.name
+            };
+        }
+        default: {
+            return state;
+        }
+    }
 };
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 
 console.log('currentState', currentState);
+
+store.dispatch({
+    type: 'CHANGE_NAME',
+    name: 'Mike'
+});
+
+console.log('name should be mike', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,9 +8,38 @@ var stateDefault = {
     todos: []
 };
 var reducer = (state = stateDefault, action) => {
-    return state;
+    switch (action.type) {
+        case 'CHANGE_SEARCH_TEXT': {
+            return {
+                ...state,
+                searchText: action.searchText
+            };
+        }
+        case 'ADD_TODO': {
+            return {
+                ...state
+            };
+        }
+        case 'REMOVE_TODO': {
+            return {
+                ...state
+            };
+        }
+        case 'CHANGE_SHOW_COMPLETED': {
+            return {
+                ...state
+            };
+        }
+        default: {
+            return state;
+        }
+    }
 };
 
 var store = redux.createStore(reducer);
-
+console.log('previousState', store.getState());
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'fuck react'
+});
 console.log('currentState', store.getState());
