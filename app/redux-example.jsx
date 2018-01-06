8abbda0e16a548a36877a1d62491220ba879da94
Mike Sharp <msharp23@hotmail.co.uk>
2018-01-06 12:13:35 +0000

    added reducer and start redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,42 +1,12 @@
 var redux = require('redux');
 
-// pure function
-function add(a, b) {
-    return a + b;
-}
+console.log('starting redux...');
 
-// non-pure function (external dependency)
-var a = 3;
-function add2 (b) {
-    return a + b;
-}
-
-var result;
-
-// side effects
-function add3 (a, b) {
-    result = a + b;
-    return result;
-}
-
-// relies on changing/temporal data, so is not pure
-function add4(a, b) {
-    return a + b + new Date().getSeconds();
-}
-
-function changeProp(obj) {
-    return {
-        ...obj,
-        name: 'fart'
-    };
-}
-
-var startingValue = {
-    name: 'mike', 
-    age: 26
+var reducer = (state = {name: 'Anonymous'}, action) => {
+    return state;
 };
+var store = redux.createStore(reducer);
 
-var res = changeProp(startingValue);
+var currentState = store.getState();
 
-console.log(startingValue);
-console.log(res);
+console.log('currentState', currentState);
